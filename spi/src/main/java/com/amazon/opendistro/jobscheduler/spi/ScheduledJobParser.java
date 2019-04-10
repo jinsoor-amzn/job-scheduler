a46acf7ddc62313d3f6b744e0f7d781134a2d627
zengyan <zengyan@amazon.com>
2019-04-09 21:53:11 -0700

    Rename package

--- spi/src/main/java/com/amazon/opendistro/jobscheduler/spi/ScheduledJobParser.java
+++
@@ -1,9 +0,0 @@
-package com.amazon.opendistro.jobscheduler.spi;
-
-import org.elasticsearch.common.xcontent.XContentParser;
-
-import java.io.IOException;
-
-public interface ScheduledJobParser {
-    ScheduledJobParameter parse(XContentParser xContentParser, String id, Long version) throws IOException;
-}
